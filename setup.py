11860eaabaad99277596c34272899cc096e37a02
Gitvidor <vdr573045093@hotmail.com>
2023-11-15 21:49:28 +0800

    update at 2023-11-15 21:49:28

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wcglib',
+    version='0.1.0',
+    author='vdr',
+    author_email='vdr573045093@hotmail.com',
+    description='wcglib is a package about data science',
+    packages=find_packages(),
+    install_requires=[
+        # List any dependencies here
+    ],
+)
+
