e0b85b8201a607b428458bd31526d1d6c6b65196
Gitvidor <vdr573045093@hotmail.com>
2023-11-15 22:28:29 +0800

    update at 2023-11-15 22:28:29

--- log_utils.py
+++ log_utils.py
@@ -4,11 +4,13 @@
 import logging
 import sys, os
 
+def logger(run_dir):
+    logger = logging.getLogger(run_dir)
 
-logger = logging.getLogger(dir_name)
+    logger.setLevel(logging.INFO)
+    handler = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s : %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
 
-logger.setLevel(logging.INFO)
-handler = logging.StreamHandler(sys.stdout)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s : %(message)s')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+    return logger
