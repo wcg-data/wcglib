61e41cbe2d57a307b13ac76d182d0e0cde731384
Gitvidor <vdr573045093@hotmail.com>
2023-11-15 22:00:30 +0800

    update at 2023-11-15 22:00:30

---
+++ wcg.py
@@ -0,0 +1,14 @@
+
+
+class WCG:
+    def get_os(self):
+        os_name = platform.system()
+        if os_name == 'Windows':
+            return 'Windows'
+        elif os_name == 'Linux':
+            return 'Linux'
+        elif os_name == 'Darwin':
+            return 'Mac'
+        else:
+            return 'Unknown'
+        
