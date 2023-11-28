643fd2f839f9ede4bd0d8ec5d7316daa83398753
Gitvidor <vdr573045093@hotmail.com>
2023-11-28 14:31:08 +0800

    update at 2023-11-28 14:31:08

--- setup.py
+++ setup.py
@@ -103,9 +103,9 @@ setup(
     author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
     url=URL,
-    packages=find_packages(exclude=["wcg.*", "log_utils"]),
+    # packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
     # If your package is a single module, use this instead of 'packages':
-    # py_modules=['wcg'],
+    py_modules=['wcg','log_utils'],
 
     # entry_points={
     #     'console_scripts': ['mycli=mymodule:cli'],
